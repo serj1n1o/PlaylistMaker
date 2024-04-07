8d38d46e0e8ad652f85e92cbad36e290db63a5c3
Sergey Bryukhanov <serjiniobryukhanov@gmail.com>
2024-04-07 21:38:30 +0300

    Добавлена функциональность по проигрыванию отрезка трека на экрана аудиоплеера AudioPlayer с помощью класса MediaPlayer, реализован таймер длительности воспроизведения.

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -5,10 +5,11 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -29,8 +30,14 @@ class SearchActivity : AppCompatActivity() {
         const val INPUT_TEXT_DEFAULT = ""
         const val PLACEHOLDER_CODE_NO_INTERNET = 0
         const val PLACEHOLDER_CODE_NOT_VISIBLE = -1
+        private const val SEARCH_DEBOUNCE_DELAY = 2000L
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
     }
 
+    private val binding by lazy { ActivitySearchBinding.inflate(layoutInflater) }
+    private var isClickAllowed = true
+    private val handlerMain = Handler(Looper.getMainLooper())
+    private val searchRunnable = Runnable { requestToTracks(inputText) }
     private var inputText = INPUT_TEXT_DEFAULT
     private val listTracks = mutableListOf<Track>()
     private val trackAdapter by lazy { TrackAdapter<Any>(listTracks) }
@@ -47,14 +54,13 @@ class SearchActivity : AppCompatActivity() {
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         searchHistory = SearchHistory(sharedPreferencesSearchHistory)
         searchHistory.loadHistoryList()
 
         if (SearchHistory.historyList.isNotEmpty() && binding.inputSearch.hasFocus()) {
-            historyShow(binding)
+            historyShow()
         }
 
         val retrofit = Retrofit.Builder().baseUrl(getString(R.string.base_url_itunes))
@@ -66,14 +72,14 @@ class SearchActivity : AppCompatActivity() {
         }
 
         binding.iconClearInput.setOnClickListener {
-            binding.apply {
+            with(binding) {
                 inputSearch.setText(INPUT_TEXT_DEFAULT)
                 inputSearch.clearFocus()
-                showPlaceHolder(PLACEHOLDER_CODE_NOT_VISIBLE, binding)
+                showPlaceHolder(PLACEHOLDER_CODE_NOT_VISIBLE)
             }
             listTracks.clear()
             trackAdapter.notifyDataSetChanged()
-            hideKeyboard(binding)
+            hideKeyboard()
         }
 
         val inputTextWatcher = object : TextWatcher {
@@ -83,33 +89,25 @@ class SearchActivity : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 binding.iconClearInput.visibility = clearInputButtonVisibility(s)
                 inputText = s.toString()
-
                 if (s.isNullOrEmpty() && binding.inputSearch.hasFocus() && SearchHistory.historyList.isNotEmpty()) {
-                    historyShow(binding)
-                    showPlaceHolder(PLACEHOLDER_CODE_NOT_VISIBLE, binding)
-                } else binding.historyListView.isVisible = false
-
+                    showPlaceHolder(PLACEHOLDER_CODE_NOT_VISIBLE)
+                    historyShow()
+                } else {
+                    binding.historyListView.isVisible = false
+                    searchDebounce()
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
             }
         }
-        binding.apply {
+        with(binding) {
             inputSearch.addTextChangedListener(inputTextWatcher)
 
             inputSearch.setOnFocusChangeListener { _, hasFocus ->
                 if (hasFocus && SearchHistory.historyList.isNotEmpty()) {
-                    historyShow(binding)
+                    historyShow()
                 } else historyListView.isVisible = false
-            }
-
-            inputSearch.setOnEditorActionListener { _, actionId, _ ->
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    requestToTracks(inputText, binding)
-                    hideKeyboard(binding)
-                }
-
-                false
             }
         }
 
@@ -117,11 +115,11 @@ class SearchActivity : AppCompatActivity() {
             searchHistory.saveTrackToHistory(it)
             historyTrackAdapter.notifyItemInserted(0)
             historyTrackAdapter.notifyItemRangeChanged(0, SearchHistory.historyList.size)
-            startAudioPlayer(it)
+            if (clickDebounce()) startAudioPlayer(it)
         }
 
         historyTrackAdapter.onItemClickListener = {
-            startAudioPlayer(it)
+            if (clickDebounce()) startAudioPlayer(it)
         }
 
         binding.btnClearHistory.setOnClickListener {
@@ -140,11 +138,12 @@ class SearchActivity : AppCompatActivity() {
         })
     }
 
-    private fun historyShow(binding: ActivitySearchBinding) {
-        binding.apply {
+    private fun historyShow() {
+        with(binding) {
             recyclerSearchTrack.isVisible = false
             recyclerHistoryList.adapter = historyTrackAdapter
             historyListView.isVisible = true
+            recyclerHistoryList.isVisible = true
         }
     }
 
@@ -152,39 +151,49 @@ class SearchActivity : AppCompatActivity() {
         return if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
     }
 
-    private fun requestToTracks(searchText: String, binding: ActivitySearchBinding) {
-        itunesAPI.getTrack(searchText).enqueue(object : Callback<ItunesResponse> {
-            @SuppressLint("NotifyDataSetChanged")
-            override fun onResponse(
-                call: Call<ItunesResponse>,
-                response: Response<ItunesResponse>
-            ) {
-                listTracks.clear()
-                binding.recyclerSearchTrack.isVisible = true
-                showPlaceHolder(PLACEHOLDER_CODE_NOT_VISIBLE, binding)
-                if (response.isSuccessful) {
-                    if (response.body()?.tracks?.isNotEmpty() == true) {
-                        listTracks.addAll(response.body()?.tracks!!)
-                        trackAdapter.notifyDataSetChanged()
-                        binding.recyclerSearchTrack.adapter = trackAdapter
-                    } else {
-                        showPlaceHolder(response.code(), binding)
+    private fun requestToTracks(searchText: String) {
+        if (searchText.isNotEmpty()) {
+            with(binding) {
+                recyclerSearchTrack.isVisible = false
+                recyclerHistoryList.isVisible = false
+                progressBar.isVisible = true
+            }
+            itunesAPI.getTrack(searchText).enqueue(object : Callback<ItunesResponse> {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onResponse(
+                    call: Call<ItunesResponse>,
+                    response: Response<ItunesResponse>
+                ) {
+                    listTracks.clear()
+                    binding.progressBar.isVisible = false
+                    binding.recyclerSearchTrack.isVisible = true
+                    showPlaceHolder(PLACEHOLDER_CODE_NOT_VISIBLE)
+                    if (response.isSuccessful) {
+                        if (response.body()?.tracks?.isNotEmpty() == true) {
+                            listTracks.addAll(response.body()?.tracks!!)
+                            trackAdapter.notifyDataSetChanged()
+                            binding.recyclerSearchTrack.adapter = trackAdapter
+                        } else {
+                            showPlaceHolder(response.code())
+                        }
                     }
                 }
-            }
 
-            override fun onFailure(call: Call<ItunesResponse>, t: Throwable) {
-                showPlaceHolder(PLACEHOLDER_CODE_NO_INTERNET, binding)
-                t.printStackTrace()
-            }
 
-        })
+                override fun onFailure(call: Call<ItunesResponse>, t: Throwable) {
+                    binding.progressBar.isVisible = false
+                    showPlaceHolder(PLACEHOLDER_CODE_NO_INTERNET)
+                    t.printStackTrace()
+                }
+
+            })
+        }
     }
 
-    private fun showPlaceHolder(code: Int, binding: ActivitySearchBinding) {
+    private fun showPlaceHolder(code: Int) {
         when (code) {
             in 200..300 -> {
-                binding.apply {
+                with(binding) {
                     recyclerSearchTrack.isVisible = false
                     historyListView.isVisible = false
                     buttonUpdate.isVisible = false
@@ -194,17 +203,31 @@ class SearchActivity : AppCompatActivity() {
             }
 
             -1 -> binding.placeholderError.isVisible = false
-            else -> binding.apply {
+            else -> with(binding) {
                 recyclerSearchTrack.isVisible = false
                 historyListView.isVisible = false
                 textError.text = getString(R.string.error_found_txt)
                 buttonUpdate.isVisible = true
                 placeholderError.isVisible = true
                 buttonUpdate.setOnClickListener {
-                    requestToTracks(inputText, binding)
+                    if (clickDebounce()) requestToTracks(inputText)
                 }
             }
         }
+    }
+
+    private fun clickDebounce(): Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handlerMain.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
+    }
+
+    private fun searchDebounce() {
+        handlerMain.removeCallbacks(searchRunnable)
+        handlerMain.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -222,7 +245,7 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-    private fun hideKeyboard(binding: ActivitySearchBinding) {
+    private fun hideKeyboard() {
         val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputManager.hideSoftInputFromWindow(binding.inputSearch.windowToken, 0)
     }
